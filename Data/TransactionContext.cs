8c7c5ee2fd87f53869ec50f868de3562002365d8
Pongsakorn Mukdavannakorn <pongsakorn_m@softsquaregroup.com>
2025-02-13 22:20:45 +0700

    adjust project structure, install nessesery packages and implement EF Core DbContext for connect DB

---
+++ Data/TransactionContext.cs
@@ -0,0 +1,12 @@
+﻿using Assignment.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment.API.Data
+{
+    public class TransactionContext : DbContext
+    {
+        public TransactionContext(DbContextOptions<TransactionContext> options) : base(options) { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignment.API.Models
+{
+    public class Transaction
+    {
+        public string Id { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal Amount { get; set; }
+        public string CurrencyCode { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string Status { get; set; }
+    }
+}
